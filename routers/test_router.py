9596a89a363c7462a11cedd6a2b446a0178bda8e
najongjine <najongjin3@hotmail.com>
2026-04-28 11:05:54 +0900

    .

--- routers/test_router.py
+++ routers/test_router.py
@@ -14,12 +14,10 @@ def test_home():
 def test_info(name: str = "", age: int = 0):
     try:
         # 비즈니스 로직 처리
-        result_data = {
+        data = {
             "name": name,
-            "age": age,
-            "message": f"안녕하세요 {name}님, 나이는 {age}세군요!"
+            "age": age
         }
-        
-        return CommonResponse(success=True, data=result_data)
+        return CommonResponse(success=True, data=data)
     except Exception as e:
         return CommonResponse(success=False, msg=str(e))
