96acaf8d868ec3b1f98df827a0dc92233e194d24
najongjine <najongjin3@hotmail.com>
2026-04-28 10:40:00 +0900

    .

--- routers/test_router.py
+++ routers/test_router.py
@@ -10,5 +10,9 @@ def test_home():
     return {"message": "Test Router Home"}
 
 @router.get("/info")
-def test_info():
-    return {"info": "This is a test router"}
+def test_info(name: str="", age: int = 0):
+    return {
+        "name": name,
+        "age": age,
+        "message": f"안녕하세요 {name}님, 나이는 {age}세군요!"
+    }
