c0342a1dbc9587b14cee1bc062b5798c0a6865db
najongjine <najongjin3@hotmail.com>
2026-04-28 11:35:29 +0900

    .

--- routers/test_router.py
+++ routers/test_router.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from utils.common import CommonResponse
 
 router = APIRouter(
     prefix="/test",
     tags=["test"]
 )
+
+class TestPostRequest(BaseModel):
+    username: str = ""
+    password: str = ""
 
 @router.get("/")
 def test_home():
@@ -20,6 +25,30 @@ def test_info(name: str = "", age: int = 0):
         data = {
             "name": name,
             "age": age
+        }
+        return CommonResponse(success=True, data=data)
+    except Exception as e:
+        return CommonResponse(success=False, msg=str(e))
+
+@router.get("/mytest")
+def mytest(username: str = "", password: str=""):
+    try:
+        # 비즈니스 로직 처리
+        data = {
+            "username": username,
+            "password": password
+        }
+        return CommonResponse(success=True, data=data)
+    except Exception as e:
+        return CommonResponse(success=False, msg=str(e))
+
+@router.post("/mytest")
+def post_mytest(request: TestPostRequest):
+    try:
+        # business logic
+        data = {
+            "username": request.username,
+            "password": request.password
         }
         return CommonResponse(success=True, data=data)
     except Exception as e:
